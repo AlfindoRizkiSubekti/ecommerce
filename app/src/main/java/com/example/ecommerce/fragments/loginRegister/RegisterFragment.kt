37d3364c23318c1133f305a49582232bac68d8bb
Alfindo Rizki Subekti <77558020+AlfindoRizkiSubekti@users.noreply.github.com>
2023-11-27 00:10:25 +0700

    add navigation animation

--- app/src/main/java/com/example/ecommerce/fragments/loginRegister/RegisterFragment.kt
+++ app/src/main/java/com/example/ecommerce/fragments/loginRegister/RegisterFragment.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.example.ecommerce.R
 import com.example.ecommerce.data.User
 import com.example.ecommerce.databinding.ActivityLoginRegisterBinding
@@ -38,6 +39,10 @@ class RegisterFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.tvHaveAccountLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
 
         binding.apply {
             buttonRegister.setOnClickListener {
