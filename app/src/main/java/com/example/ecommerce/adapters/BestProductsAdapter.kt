223fe76a5a065344bf705be34f3bf60c6f336e06
Alfindo <rizkisubekti19@gmail.com>
2024-01-10 23:54:39 +0700

    Add Cart ViewModel, Adapter & Add Fitur notifikasi Cart

--- app/src/main/java/com/example/ecommerce/adapters/BestProductsAdapter.kt
+++ app/src/main/java/com/example/ecommerce/adapters/BestProductsAdapter.kt
@@ -10,24 +10,24 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.ecommerce.data.Product
 import com.example.ecommerce.databinding.ProductRvItemBinding
+import com.example.ecommerce.helper.getProductPrice
 
 class BestProductsAdapter : RecyclerView.Adapter<BestProductsAdapter.BestProductsViewHolder>() {
     inner class BestProductsViewHolder(private val binding: ProductRvItemBinding) :
         RecyclerView.ViewHolder(binding.root) {
         fun bind(product: Product) {
             binding.apply {
-                Glide.with(itemView).load(product.images[0]).into(imgProduct)
-                product.offerPercentage?.let {
-                    val remainingPricePrecentage = 1f - it
-                    val priceAfterOffer = remainingPricePrecentage * product.price
-                    tvNewPrice.text = "$ ${String.format("%.2f", priceAfterOffer)}"
-                    tvPrice.paintFlags = Paint.STRIKE_THRU_TEXT_FLAG
-                }
+                val priceAfterOffer = product.offerPercentage.getProductPrice(product.price)
+                tvNewPrice.text = "$ ${String.format("%.2f", priceAfterOffer)}"
+                tvPrice.paintFlags = tvPrice.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
                 if (product.offerPercentage == null)
                     tvNewPrice.visibility = View.INVISIBLE
+
+                Glide.with(itemView).load(product.images[0]).into(imgProduct)
                 tvPrice.text = "$ ${product.price}"
                 tvName.text = product.name
             }
+
         }
     }
 
