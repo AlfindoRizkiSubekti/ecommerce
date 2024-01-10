d1e05fe94894d25dc4382cc0753137d04e9fe494
Alfindo <rizkisubekti19@gmail.com>
2024-01-10 11:57:09 +0700

    add setup untuk setiap kategori

--- app/src/main/java/com/example/ecommerce/fragments/categories/BaseCategoryFragment.kt
+++ app/src/main/java/com/example/ecommerce/fragments/categories/BaseCategoryFragment.kt
@@ -5,25 +5,54 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.NestedScrollView
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ecommerce.R
+import com.example.ecommerce.adapters.BestProductsAdapter
+import com.example.ecommerce.databinding.FragmentBaseCategoryBinding
 import com.example.ecommerce.databinding.FragmentMainCategoryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class BaseCategoryFragment : Fragment(R.layout.fragment_main_category) {
-
-    private lateinit var binding: FragmentMainCategoryBinding
+open class BaseCategoryFragment: Fragment(R.layout.fragment_base_category) {
+    private lateinit var binding: FragmentBaseCategoryBinding
+    private lateinit var offerAdapter: BestProductsAdapter
+    private lateinit var bestProductsAdapter: BestProductsAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentMainCategoryBinding.inflate(inflater)
+    ): View? {
+        binding = FragmentBaseCategoryBinding.inflate(inflater)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        setupOfferRv()
+        setupBestProductsRv()
+
     }
+
+    private fun setupBestProductsRv() {
+        binding.rvBestProducts.apply {
+            layoutManager =
+                GridLayoutManager(requireContext(), 2, GridLayoutManager.VERTICAL, false)
+            adapter = bestProductsAdapter
+        }
+    }
+
+    private fun setupOfferRv() {
+        binding.rvOfferProducts.apply {
+            layoutManager =
+                LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+            adapter = offerAdapter
+        }
+    }
+
 }
